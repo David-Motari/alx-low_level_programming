c59561f68b2409741f02e86750bec19e3e94bcbe
under-ground-dog <davemotari22@gmail.com>
2022-03-10 12:46:13 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
-# include <stdio.h>
-
+#include <stdio.h>
+/**
+ * main - main block
+ *Return: 0
+ */
 int main ()
 {
   printf("with proper grammar, but the outcome is a piece of art,/n");
-    return 0;
+  return (0);
 }
