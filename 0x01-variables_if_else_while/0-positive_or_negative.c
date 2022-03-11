663c43d7e3e747f7b514dac2238f83c6b0057bc2
under-ground-dog <davemotari22@gmail.com>
2022-03-11 12:14:22 -0800

    Positive, zero or negative conditional

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -26,13 +26,13 @@ int main(void)
   n = rand() - RAND_MAX / 2;
   
   
-  if (n>0){
-  printf("is positive \n");
- } else if (n=0){
-  printf("is zero \n");
- } else if (n<0){
-  printf("is negative \n");
- }
+  if (n > 0)
+             printf("is positive \n");
+  else if (n = 0)
+             printf("is zero \n");
+  else
+             printf("is negative \n");
+ 
   return (0);
   
 }
