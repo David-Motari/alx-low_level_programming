2edc963b159756beb9cf9fdc7df471e341d7ee9a
under-ground-dog <davemotari22@gmail.com>
2022-03-15 13:54:46 -0700

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet x10 - prints 10 times the alphabet, in lowcase. 
+*
+*
+*Return: void
+*/
+void print_alphabet(void)
+{
+int i;
+char mot;
+for (i = 0; i < 10; i++)
+{
+for (mot = 'a'; mot <= 'z'; mot++);
+_putchar(mot);
+}
+_putchar('\n')
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 File 0 prints _putchar
 File 1 prints the alphabet, in lowercase, followed by a new line.
+prints 10 times the alphabet, in lowercase, followed by a new line.
