dd3aad0739d98c5f7a0fee19b9bc192de6ab143a
under-ground-dog <davemotari22@gmail.com>
2022-03-16 13:07:41 -0700

    Check number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_sign - checks if sign on number is postive or negative.
+ *@n: integer to be assessed
+ *
+ *Checks sign of number positive or negative.
+ *Return: 0
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ File 1 prints the alphabet, in lowercase, followed by a new line.
 prints 10 times the alphabet, in lowercase, followed by a new line.
 File 3 function that checks for lowercase character.
 file 4  function that checks for alphabetic character.
+function that prints the sign of a number.
