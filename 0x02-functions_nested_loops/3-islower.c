bca8be0e233b2ff0ebf9307d9d3699c859a9f458
under-ground-dog <davemotari22@gmail.com>
2022-03-16 10:44:38 -0700

    Check for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,11 +6,10 @@
 *_islower - if lowercase character and prints 1 and 0 for otherwise.
 *
 *Checks if lowercase character
-*Returns: Always 0 
-*
+*Returns: Always 0
 */
 
 int _islower(int c)
 {
-  return (c >= 'a' && c <= 'z');
+return (c >= 'a' && c <= 'z');
 }
