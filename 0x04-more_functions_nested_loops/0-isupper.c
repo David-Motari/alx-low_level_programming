6eb6eb7256d91dd4878cf984e48491b0c030bf44
under-ground-dog <davemotari22@gmail.com>
2022-03-17 13:34:35 -0700

    Check for uppercase xter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_isupper - Returns 1 if c is uppercase Returns 0 otherwise
+*
+*checks for uppercase character.
+*Return: 0
+*/
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
