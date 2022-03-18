49a9c79b2de64eb546c7fdf5b5e20599e901fbd1
under-ground-dog <davemotari22@gmail.com>
2022-03-17 21:51:18 -0700

    Multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul - multiplies two integers
+*@a: first variable
+*@b: second variable
+*
+*(int a, int b)
+*Return: 0
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 File 0 checks for uppercase character
 file 1 - checks for digit (0-9)
+file 2 multiplies two integers
