8c1718b7bd0716d248528411348bdc8f89735d26
under-ground-dog <davemotari22@gmail.com>
2022-03-21 01:34:42 +0000

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ *print_most_numbers - prints 1 - 9 without 2 & 4
+ *&i: integer
+ *
+ *Prints one to nine, skips 2 & 4 
+ *Return: 0
+ */
 void print_most_numbers(void)
 {
-int i;
+char i;
 for (i = 0; i < 9; i++)
 if (i == 2 || i == 4)
 {
