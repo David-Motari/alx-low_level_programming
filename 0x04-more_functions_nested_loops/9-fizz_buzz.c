fb3666233368bac71048316205b1996c7ee06a68
under-ground-dog <davemotari22@gmail.com>
2022-03-21 05:08:05 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@
 int main(void)
 {
 int i;
-for (i = 0; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
-printf("Fizz");
+printf(" Fizz");
 
 else if (i % 5 == 0)
-printf("Buzz");
+printf(" Buzz");
 
 else if (i % 15 == 0)
 printf("FizzBuzz");
 
-else 
-printf ("%i", i);
+else
+printf(" %i ", i);
 }
 
 printf("\n");
