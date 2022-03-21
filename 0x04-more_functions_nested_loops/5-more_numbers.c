089b7e908aa973335160763458e01d361325f26a
under-ground-dog <davemotari22@gmail.com>
2022-03-21 02:15:10 +0000

    Numbers in lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 1 - 14, x10
+*@i: variable
+*@j: variable
+*
+*Prints 1 - 14 ten times
+*Return: 0
+*/
+
+void more_numbers(void)
+{
+char i, j;
+i = 0;
+while (i < 10)
+{
+j = 0;
+while (j < 14)
+{
+_putchar(j);
+j++;
+}
+_putchar('\n');
+i++;
+}
+_putchar('\n');
+}
+
