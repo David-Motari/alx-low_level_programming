62d1a59daf131a2674b6f403696a1d73107b079c
under-ground-dog <davemotari22@gmail.com>
2022-03-21 18:13:13 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,10 +18,10 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
-printf("Fizz ");
+printf("Fizz  ");
 
 else if (i % 5 == 0)
-printf(" Buzz ");
+printf(" Buzz  ");
 
 else if (i % 15 == 0)
 printf("FizzBuzz ");
