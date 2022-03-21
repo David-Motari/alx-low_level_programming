e065a2876c546547723cf770c3de3a26503fe41e
under-ground-dog <davemotari22@gmail.com>
2022-03-21 21:41:32 +0000

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_rev - prints str in stdout but reverse
+ *@s: strint to be printed
+ *
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+int i;
+
+while (*(s + i))
+i++;
+i = i - 1;
+while (1 >= 0)
+{
+_putchar(*(s + i));
+i--;
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,4 +1,7 @@
 This directory has files on Arrays Pointers and Strings
 file 0 - returns value of c to 98
 file 1 swaps values of two variables
+File 2 Returns length of string
+file 3 Prints screen in standard output 
+file 4 Reverses order of string
 
