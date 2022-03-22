6c3ee138a36ebd160c672eae370a4d97f160b5e7
under-ground-dog <davemotari22@gmail.com>
2022-03-22 04:48:10 +0000

    prints xter of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*puts2 - prints every other xter of a string, starting with 1st
+*@str: string
+*
+*Return: 0
+*/
+
+void puts2(char *str)
+{
+int i = 0;
+
+while (*(str + i))
+{
+_putchar(*(str + i));
+i = i + 2;
+}
+_putchar('\n');
+}
+
