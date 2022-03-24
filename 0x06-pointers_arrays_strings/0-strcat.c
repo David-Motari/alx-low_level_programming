78461b6eb9424d96dd110250057b6d3a39d38bb8
under-ground-dog <davemotari22@gmail.com>
2022-03-24 19:09:57 +0000

    Concate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,16 @@ char *_strcat(char *dest, char *src)
 int i, j;
 
 i = j = 0;
-while (*(dest + i))
+
+while (dest [i] != '\0')
 i++;
-while ((*(dest + i) = *(src + j)))
+while (src[j] != '\0')
 {
+dest[i] = src[j];
 i++;
 j++;
 }
+dest[i] = '\0';
 return (dest);
 }
 
