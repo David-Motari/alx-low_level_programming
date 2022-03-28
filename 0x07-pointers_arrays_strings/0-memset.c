bedf3acc4d0c57daccc2558209ab48da0ed32369
under-ground-dog <davemotari22@gmail.com>
2022-03-28 19:58:18 +0000

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /*
 *_memset - first n bytes of the memory pointed by s with constant byte c.
-*@s:pointer to memory
+*@s: pointer to memory
 *@b: const value
 *@n: bytes to fill 
 *
@@ -12,13 +12,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-int size = n;
-if (size > 0) 
-{
-int i;
-for (i = 0; 1 < size; i++)
-*(s + i ) = b;
-}
+unsigned int i;
+for (i = 0; i < n; i++)
+s[i] = b;
 return (s);
 }
 
