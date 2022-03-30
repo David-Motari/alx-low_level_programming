92f30d2d3d9cf6cad0c2a2d4cd21c1b8d3f3e756
under-ground-dog <davemotari22@gmail.com>
2022-03-30 19:33:18 +0000

    Reverse string

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - prints reverse of string
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+_print_rev_recursion("\nColton Walker");
+return (0);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,3 @@
 Recursion
 File 0 prints string using recursion
+File 1 reverses string
