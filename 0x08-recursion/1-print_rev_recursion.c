c467d6f7ad0f210781e1a3eb9c9abec3175dc43e
under-ground-dog <davemotari22@gmail.com>
2022-03-31 03:08:19 +0000

    Reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
+/**
 *_print_rev_recursion - prints reversed string
- *@s: string to reverse
- *
- * Return: 0
- */
+*@s: string to reverse
+*
+* Return: 0
+*/
 void _print_rev_recursion(char *s)
 {
 if (*s)
