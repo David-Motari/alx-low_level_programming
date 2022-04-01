e50352cf710027ffcea23d719d789f293263ee26
under-ground-dog <davemotari22@gmail.com>
2022-04-01 20:55:16 +0000

    No of args passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - prnts no of args passed
+*@argc: no of args
+*@argv: array
+*
+*Return: 
+*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
