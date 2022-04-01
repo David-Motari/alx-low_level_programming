2b5dd39b35a720966f191f4f66a54ef2bb8ba98a
under-ground-dog <davemotari22@gmail.com>
2022-04-01 21:02:36 +0000

    No of args passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,9 @@
 *@argc: no of args
 *@argv: array
 *
-*Return: 
+*Return: 0
 */
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[] __attribute__((__unused__)))
 {
 printf("%d\n", argc - 1);
 return (0);
