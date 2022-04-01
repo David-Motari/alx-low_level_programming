db54bd61ca53f55898914cbad103816792113ac9
under-ground-dog <davemotari22@gmail.com>
2022-04-01 20:29:41 +0000

    What's My Name?!

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,14 +17,14 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-printf("Fizz");
+if (i % 15 == 0)
+printf("BuzzFizz");
+
+else if (i % 3 == 0)
+printf(" Fizz");
 
 else if (i % 5 == 0)
-printf(" Buzz");
-
-else if (i % 15 == 0)
-printf("FizzBuzz");
+printf("Buzz");
 
 else
 printf("%i", i);
