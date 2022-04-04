2bcab549315ac67d37c1063ee1ecb69a7f61a78e
under-ground-dog <davemotari22@gmail.com>
2022-04-04 20:00:26 +0000

    returns a pointer to duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-*_strdup - returns pointer to a new string 
+*_strdup - returns pointer to a new string
 *that is a duplictate of string s
 *@s: string to be duplicated
 *
@@ -18,7 +18,7 @@ int i, l = 0;
 if (s == NULL)
 return (NULL);
 
-for (i =0; s[i]; i++)
+for (i = 0; s[i]; i++)
 l++;
 
 duplicate = malloc(sizeof(char) * (l + 1));
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 Memory Allocation 
 file 0 creates an array of chars and intializes it with specific char
+file 1 returns pointer to new string that is duplicate of s
