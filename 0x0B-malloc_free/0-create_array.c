7e2d6fa13be4a1704c97fda671ed78b585ae5298
under-ground-dog <davemotari22@gmail.com>
2022-04-04 18:54:40 +0000

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*create_array - generates an array of xters
+*@size: size of array
+*@c: xter to initialize array
+*
+*Return: if size == 0; of error - Null alse pointer to array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int i;
+
+if (size == 0)
+return (NULL);
+
+array = malloc(sizeof(char) * size);
+if (array == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+array[i] = c;
+
+return(array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+Memory Allocation 
+file 0 creates an array of chars and intializes it with specific char
