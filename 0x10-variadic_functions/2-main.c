ff3d515dd91efc4e33c0872a3b95502a1e3c6444
under-ground-dog <davemotari22@gmail.com>
2022-04-15 20:02:02 +0000

    Prints string

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+* main - calls function that prints string
+*
+* Return: 0.
+*/
+int main(void)
+{
+print_strings(", ", 2, "Jay", "Django");
+return (0);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,4 @@
 Variadic Functions
 File 0 sums all parameters
 File 1 prints numbers
+File 3 prints string
