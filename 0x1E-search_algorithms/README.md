3d959d08eefbdfc2cb797113343bcda21f977636
David Motari <davemotari22@gmail.com>
2022-10-05 06:40:00 +0000

    Task 0

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,4 @@
+# 0x1E. C - Search Algorithms
+
+## File 0 - Linear search:
+* [0-linear.c](./0-linear.c) - searches for a value in an array of integers using the Linear search algorithm. 
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+/*Library Headers*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Prototypes*/
+int linear_search(int *array, size_t size, int value);
+
+#endif /*_SEARCH_ALGOS_H*/
